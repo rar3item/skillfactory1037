38aac5d0c40ed5a049038ebef43514954cc106b8
rar3item <36587939+rar3item@users.noreply.github.com>
2022-11-15 11:59:25 +0500

    Add files via upload

---
+++ 12.7 Ryazansky.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposits = []
+money = int(input())
+for each in per_cent:
+    deposits.append(int(per_cent[each] * money * 0.01))
+print(deposits)
+print('Максимальная сумма которую вы можете заработать -' max(deposits))
